df66728254165bc9af5e8055536acd67c77325ec
thanh <capslock1131@gmail.com>
2020-03-27 08:00:51 +0700

    add osb project

---
+++ react-boilerplate-master/app/components/MainLayout/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * MainLayout
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function MainLayout() {
+  return <div />;
+}
+
+MainLayout.propTypes = {};
+
+export default MainLayout;
